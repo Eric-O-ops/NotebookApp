a025e92d29a4535ee6aa225b16050555cd2cdaf7
Omaraliev Eric <theslyfox43@gmail.com>
2023-01-07 23:17:18 +0600

    added functions to get background color of note and title from description

--- app/src/main/java/com/eric/notebook/ui/fragments/main/addnote/AddNoteFragment.kt
+++ app/src/main/java/com/eric/notebook/ui/fragments/main/addnote/AddNoteFragment.kt
@@ -13,6 +13,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class AddNoteFragment : BaseFragment<FragmentAddNoteBinding>(R.layout.fragment_add_note) {
 
     override val binding by viewBinding(FragmentAddNoteBinding::bind)
+    private var noteBackgroundColor = "#191818"
+    private var title = ""
 
     override fun setupListener() {
 
@@ -27,6 +29,7 @@ class AddNoteFragment : BaseFragment<FragmentAddNoteBinding>(R.layout.fragment_a
 
     private fun createNote() {
         binding.btnCreateNote.setOnClickListener {
+
             navigate(R.id.action_addNoteFragment_to_mainFragment)
         }
     }
@@ -34,14 +37,25 @@ class AddNoteFragment : BaseFragment<FragmentAddNoteBinding>(R.layout.fragment_a
     private fun setBackgroundColorNote() = with(binding) {
         btnColor1.setOnClickListener {
             radioBtnPosition1.isChecked = true
+            noteBackgroundColor = "#191818"
         }
 
         btnColor2.setOnClickListener {
             radioBtnPosition2.isChecked = true
+            noteBackgroundColor = "#EBE4C9"
         }
 
         btnColor3.setOnClickListener {
             radioBtnPosition3.isChecked = true
+            noteBackgroundColor = "571818"
+        }
+    }
+
+    private fun getTitle() = with(binding) {
+        val charSentence = etDescriptionNote.text.toString().toCharArray()
+
+        for (i in 0..30) {
+            title += charSentence[i].toString()
         }
     }
 }
